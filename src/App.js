1eb420fafc95bb9d31cbd292af9e1c96344bcdd8
Vedika Garg <vedika.garg@coviam.com>
2022-01-21 23:22:30 +0530

    made data dynamic

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
+import React, { useEffect, useState } from 'react';
+
 import BarChart from './Barchart';
 
 import './App.css';
 
 function App() {
 
-  const data = [
-    { title: "HR", salary: 180, compratio: 80, headcount: 100 },
-    { title: "Marketing", salary: 190, compratio: 85, headcount: 80 },
-    { title: "Engineering", salary: 200, compratio: 60, headcount: 50 },
-    { title: "Operations", salary: 130, compratio: 118, headcount: 300 },
-    { title: "Finance", salary: 225, compratio: 80, headcount: 120 },
-    { title: "Logistics", salary: 190, compratio: 95, headcount: 70 },
-    { title: "IT", salary: 140, compratio: 60, headcount: 230 },
-    { title: "Security", salary: 150, compratio: 75, headcount: 77 },
-    { title: "Legal", salary: 250, compratio: 110, headcount: 20 },
-    { title: "International", salary: 230, compratio: 110, headcount: 20 }
-  ];
+  const [data, setData] = useState([]);
+  const [loader, setLoader] = useState(true);
+  const [error, setError] = useState(false)
 
-
-
+  useEffect(() => {
+    fetch("https://mocki.io/v1/18936d28-2f79-4840-b146-5622e8ad1e77")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setLoader(false);
+          setData(result);
+        },
+        (error) => {
+          setLoader(false);
+          setError(error);
+        }
+      );
+  }, []);
 
   return (
     <div>
-      <BarChart data={data} />
+      {loader ? <div>loading please wait</div> : <BarChart data={data} />}
+
     </div>
   );
 }
